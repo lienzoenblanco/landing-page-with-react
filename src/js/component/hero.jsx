5631f89974075d040182d83f543801207f8d27b8
Raquel García <92454824+lienzoenblanco@users.noreply.github.com>
2022-01-14 18:27:38 +0100

    Firstfeature (#2)
    * fist commit
    
    * updates

---
+++ src/js/component/hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+	return (
+		<div class="jumbotron container bg-light mt-4 p-4">
+			<h1 class="display-4">Hello, world!</h1>
+			<p class="lead">
+				This is a simple hero unit, a simple jumbotron-style component
+				for calling extra attention to featured content or information.
+			</p>
+			<p class="lead">
+				<a class="btn btn-primary btn-lg" href="#" role="button">
+					Learn more
+				</a>
+			</p>
+		</div>
+	);
+};
+
+export default Hero;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Card from "./card.jsx";
 import "../../styles/card.css";
+import Menu from "./menu.jsx";
+import Hero from "./hero.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -8,9 +10,13 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="card">
-			<Card>Card title </Card>;<Card>Card title</Card>;
-			<Card>Card title</Card>;<Card>Card title</Card>;
+		<div>
+			<Menu />
+			<Hero />
+			<div className="card">
+				<Card>Card title </Card>;<Card>Card title</Card>;
+				<Card>Card title</Card>;<Card>Card title</Card>;
+			</div>
 		</div>
 	);
 };
